330872d77f0b7e90921a0b322e36b4e9259d046d
Appsdev202 <76201794+Appsdev202@users.noreply.github.com>
2021-08-30 00:46:31 +0000

    lms

---
+++ lms-pro.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Rim Cours
+Plugin URI: 
+Description: Power up LMS plugins
+Author: rimtm
+Version: 1
+Author URI: 
+Requires at least: 
+Tested up to: 
+Text Domain:
+Domain Path: /languages/
+*/
